8e0926339b71ba8cdaa904b4e6868ee1965cf262
RedPandaMC <jurreandenys@gmail.com>
2023-05-15 20:20:57 +0200

    15/05/2023

--- app.py
+++ app.py
@@ -8,54 +8,23 @@ import json
 import sys
 import os
 import re
+import yaml
 import numpy as np
 
 
-class Fietstransporteur:
+class MissingJsonFilesError(Exception):
     """
-    Deze class representeert een fietstransporteur
-    """
-
-    aantal_transporteurs = 0
-    max_transporteurs = 0
-    max_aantal_fietsen = 20
-
-    def __init__(self):
-        self._nummer = Fietstransporteur.aantal_transporteurs
-        Fietstransporteur.aantal_transporteurs += 1
-
-
-class Gebruiker:
-    """
-    Deze class representeert een gebruiker
-
-    Class attributen:
-        - aantal_gebruikers [int]
-        - max_gebruikers [int]
-
-    Instance attrbuten:
-        - _sex [str] (2 options)
-        - _voornaam [str]
-        - _achternaam [str]
-        - _aantal_ritten [int]
-        - _laatste_rit [Rit]
+    A custom error to let the user know what went wrong
     """
 
-    aantal_gebruikers = 0
-    max_gebruikers = 0
-    totaal_aantal_ritten = 0
+    def __init__(self, missing_files):
+        self.missing_files = missing_files
 
-    def __init__(self, voornaam_man: list, voornaam_vrouw: list, achternaam: list):
-        self._sex = "Man" if np.random.randint(0, 2) > 0.5 else "Vrouw"
-        self._voornaam = (
-            voornaam_man[np.random.randint(0, 101)]
-            if self._sex == "Man"
-            else voornaam_vrouw[np.random.randint(0, 101)]
+    def __str__(self):
+        return (
+            "De volgende .JSON files die nodig"
+            + f" zijn voor het setup process missen: {', '.join(self.missing_files)}"
         )
-        self._achternaam = achternaam[np.random.randint(0, 101)]
-        self._aantal_ritten = 0
-        self._laatste_rit = None
-        Gebruiker.aantal_gebruikers += 1
 
 
 class Fiets:
@@ -191,6 +160,62 @@ class Rit:
         return time_minutes
 
 
+class Gebruiker:
+    """
+    Deze class representeert een gebruiker
+
+    Class attributen:
+        - aantal_gebruikers [int]
+        - max_gebruikers [int]
+
+    Instance attrbuten:
+        - _sex [str] (2 options)
+        - _voornaam [str]
+        - _achternaam [str]
+        - _aantal_ritten [int]
+        - _laatste_rit [Rit]
+    """
+
+    aantal_gebruikers = 0
+    max_gebruikers = 0
+    totaal_aantal_ritten = 0
+
+    def __init__(self, voornaam_man: list, voornaam_vrouw: list, achternaam: list):
+        self._sex = "Man" if np.random.randint(0, 2) > 0.5 else "Vrouw"
+        self._voornaam = (
+            voornaam_man[np.random.randint(0, 101)]
+            if self._sex == "Man"
+            else voornaam_vrouw[np.random.randint(0, 101)]
+        )
+        self._achternaam = achternaam[np.random.randint(0, 101)]
+
+        self._aantal_ritten = 0
+        self._laatste_rit = None
+        Gebruiker.aantal_gebruikers += 1
+
+    def _verplaatsings_update(self, rit: Rit):
+        """
+        Deze functie update een aantal instance attributen
+        """
+        self._laatste_rit = rit
+        self._aantal_ritten += 1
+        Gebruiker.totaal_aantal_ritten += 1
+
+
+class Fietstransporteur:
+    """
+    Deze class representeert een fietstransporteur
+    """
+
+    aantal_transporteurs = 0
+    max_transporteurs = 0
+    max_aantal_fietsen = 20
+
+    def __init__(self):
+        self._nummer = Fietstransporteur.aantal_transporteurs
+        Fietstransporteur.aantal_transporteurs += 1
+
+
 class App:
     """
     Deze class representeert onze simulator app
@@ -202,11 +227,72 @@ class App:
         self._stations = []
         self._gebruikers = []
         self._fietsen = []
+        self._transporteurs = []
+        self._willekeurigheid = 0
 
-    # region parallel 1
-    def __check_dir(self, *dir_names: str) -> bool:
+    def setup(self):
         """
-        Checks if sqr_half given directory exists
+        Runs the program setup process:
+
+        Step 1
+            -> check if all directories are available
+                - input,config,libs
+            -> create missing directories if needed
+        step 2
+            -> check if all files are in each directory
+                - config: config.yaml
+                - libs: gebruikers.pickle, stations.pickle, fietsen.pickle, vervoerwagens.pickle
+                - input: names.json, velo.json if missing and gebruikers.pickle,
+                         stations.pickle does not exist ===> send error to user
+            -> load and assign config parameters
+            -> create missing files if needed
+        """
+
+        # region stap1
+        dirs_needed = ["input", "config", "libs"]
+        dirs_present = self._check_dir("input", "config", "libs")
+        dirs_to_create = self._mask_list(dirs_needed, dirs_present)
+        self._make_dir(*dirs_to_create)
+
+        if "input" in dirs_to_create and "libs" in dirs_to_create:
+            missing_files = ["names.json", "velo.json"]
+            raise MissingJsonFilesError(missing_files)
+        # endregion
+
+        # region stap2
+        config_file_present = self._check_files("config", "config.yaml")
+        if config_file_present[0] is False:
+            self._create_config()
+        self._load_config()
+
+        lib_files_needed = [
+            "gebruikers.pickle",
+            "stations.pickle",
+            "fietsen.pickle",
+            "vervoerwagens.pickle",
+        ]
+        lib_files_present = self._check_files(
+            "libs",
+            "gebruikers.pickle",
+            "stations.pickle",
+            "fietsen.pickle",
+            "vervoerwagens.pickle",
+        )
+        lib_files_to_create = self._mask_list(lib_files_needed, lib_files_present)
+        if "gebruikers.pickle" in lib_files_to_create:
+            self._create_users()
+        if "stations.pickle" in lib_files_to_create:
+            self._create_stations()
+        if "fietsen.pickle" in lib_files_to_create:
+            self._create_bikes()
+        if "vervoerwagens.pickle" in lib_files_to_create:
+            self._create_bikemovers()
+        # endregion
+
+    # region __functions-setup__
+    def _check_dir(self, *dir_names: str) -> list[bool]:
+        """
+        Checks if a given directory exists
 
         Attributes:
             dir_names [str] -> name of dir / path (allows multi input)
@@ -221,14 +307,11 @@ class App:
                 res.append(True)
             else:
                 res.append(False)
-        if len(res) == 1:
-            return res[0]
-        else:
-            return res
+        return res
 
-    def __make_dir(self, *dir_names: str) -> None:
+    def _make_dir(self, *dir_names: str) -> None:
         """
-        Creates sqr_half given dir
+        Creates a given dir
 
         Attributes:
             dir_name [str] -> names of dir / path (allows multi input)
@@ -239,61 +322,36 @@ class App:
         for dir_name in dir_names:
             os.makedirs(dir_name)
 
-    # endregion
+    def _mask_list(self, original_list: list[str], mask: list[bool]) -> list[str]:
+        masked_list = []
+        for item, mask in zip(original_list, mask):
+            if not mask:
+                masked_list.append(item)
+        return masked_list
 
-    # region parallel 2
-    def __check_input_files(self) -> bool:
+    def _check_files(self, directory: str, *files: list) -> list[bool]:
         """
-        Checks the input directory to see if input files exists,
-        if not sends sqr_half true or false
+        Checks the input directory to see if specified files exist,
+        returns True if all files exist, False otherwise.
         """
-        inputfiles = ["names.json", "velo.json"]
-        existingfiles = []
-        for file in inputfiles:
+        existing_files = []
+        for file in files:
             try:
-                with open(f"input/{file}", "r", encoding="UTF-8"):
-                    existingfiles.append(file)
+                with open(
+                    f"{directory}/{file}",
+                    "r",
+                    encoding="UTF-8",
+                ):
+                    existing_files.append(True)
             except FileNotFoundError:
-                print(f"File '{file}' does not exist, please add it.")
+                existing_files.append(False)
+        return existing_files
 
-        if len(existingfiles) == len(inputfiles):
-            return True
-        else:
-            return False
-
-    def __check_library_files(self) -> bool:
-        """
-        This function checks if all of our
-        library files are present, returns true
-        if all exists
-        """
-        inputfiles = [
-            "gebruikers.pickle",
-            "stations.pickle",
-            "fietsen.pickle",
-            "vervoerwagens.pickle",
-        ]
-        existingfiles = []
-        for file in inputfiles:
-            try:
-                with open(f"input/{file}", "rb", encoding="UTF-8"):
-                    existingfiles.append(file)
-            except FileNotFoundError:
-                print(f"File '{file}' does not exist, please add it.")
-
-        if len(existingfiles) == len(inputfiles):
-            return True
-        else:
-            return False
-
-    # endregion
-
-    # region parallel 3
-    def __create_stations(self) -> None:
+    def _create_stations(self) -> None:
         """
         This function removes unused data
         from the velo.json file
-        and dumps it into sqr_half stations.pkl file.
+        and dumps it into libs/stations.pkl file.
         """
         with open("input/velo.json", "r", encoding="UTF-8") as file:
             data = json.load(file)
@@ -317,13 +375,13 @@ class App:
                     self._stations.append(station)
                 else:
                     continue
-        with open("libs/stations.pickle", "wb", encoding="UTF-8") as newpickle:
+        with open("libs/stations.pickle", "wb") as newpickle:
             pickle.dump(self._stations, newpickle)
 
-    def __create_users(self) -> None:
+    def _create_users(self) -> None:
         """
-        This function creates sqr_half number of users specified
-        by the amount in the config file.
+        This function creates a number of 'users'
+        specified by the amount in the config file.
         """
         with open(r"input/names.json", "r", encoding="UTF-8") as file:
             data = json.load(file)
@@ -335,20 +393,91 @@ class App:
                 gebruiker = Gebruiker(voornaam_man, voornaam_vrouw, achternaam)
                 self._gebruikers.append(gebruiker)
 
-        with open("libs/gebruikers.pickle", "wb", encoding="UTF-8") as newpickle:
+        with open("libs/gebruikers.pickle", "wb") as newpickle:
             pickle.dump(self._gebruikers, newpickle)
 
-    def __create_bikes(self) -> None:
+    def _create_bikes(self) -> None:
         """
-        This function creates sqr_half number of bikes specified
+        This function creates a number of 'bikes' specified
         by the amount in the config file.
         """
         while Fiets.aantal_fietsen <= Fiets.max_fietsen:
             fiets = Fiets()
             self._fietsen.append(fiets)
 
-        with open("libs/fietsen.pickle", "wb", encoding="UTF-8") as newpickle:
+        with open("libs/fietsen.pickle", "wb") as newpickle:
             pickle.dump(self._fietsen, newpickle)
 
+    def _create_bikemovers(self) -> None:
+        """
+        This function creates a number of 'bikemovers' specified
+        by the amount in the config file
+        """
+        while (
+            Fietstransporteur.aantal_transporteurs
+            <= Fietstransporteur.max_aantal_fietsen
+        ):
+            fietstransporteur = Fietstransporteur()
+            self._transporteurs.append(fietstransporteur)
 
-# endregion
+        with open("libs/vervoerwagens.pickle", "wb") as newpickle:
+            pickle.dump(self._fietsen, newpickle)
+
+    def _create_config(self) -> None:
+        file_path = os.path.join("config", "config.yaml")
+        config_data = {
+            "max_log_bestandsgrote": 10,
+            "aantal_gebruikers": 100,
+            "aantal_fietsen": 50,
+            "aantal_transporteurs": 25,
+            "willekeurigheid": 50,
+        }
+        with open(file_path, "w", encoding="UTF-8") as config_file:
+            yaml.dump(config_data, config_file)
+        with open(file_path, "a", encoding="UTF-8") as config_file:
+            config_file.write(
+                "\n# Max log file size in megabytes\n# Randomness percentage (between 0-100)"
+            )
+
+    def _load_config(self) -> None:
+        with open("config/config.yaml", "r", encoding="UTF-8") as config_file:
+            config_data = yaml.safe_load(config_file)
+            Gebruiker.max_gebruikers = config_data["aantal_gebruikers"]
+            Fiets.max_fietsen = config_data["aantal_fietsen"]
+            Fietstransporteur.max_transporteurs = config_data["aantal_transporteurs"]
+            self._willekeurigheid = config_data["willekeurigheid"]
+            # add log class here
+
+    # endregion
+
+
+def main():
+    """
+    Processes sys.argv flags, and
+    runs the according functions
+    """
+    # region patterns
+    help_pat = r"^(-{1,2}[hH]|-{1,2}[hH]elp)$"
+    setup_pat = r"^(-{1,2}[sS]|-{1,2}[sS]etup)$"
+    run_pat = r"^(-{1,2}[rR]|-{1,2}[rR]un)$"
+    load_pat = r"^(-{1,2}[lL]|-{1,2}[lL]oad)$"
+    # endregion
+    if len(sys.argv) > 1:
+        user_flag = str(sys.argv[1])
+        if re.match(help_pat, user_flag):
+            print("Running help")
+        elif re.match(setup_pat, user_flag):
+            velosim.setup()
+        elif re.match(run_pat, user_flag):
+            print("Running run")
+        elif re.match(load_pat, user_flag):
+            print("Running load")
+        else:
+            print("Flag not recognized.")
+    else:
+        print("Please add a flag")
+
+
+if __name__ == "__main__":
+    velosim = App()
+    main()
